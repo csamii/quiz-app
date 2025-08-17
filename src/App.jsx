e060ff9ec8516fc130e83ae5155239c09491a096
csamii <csamii27@gamil.com>
2025-08-17 22:17:08 +0100

    Quiz App begins

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddRecipeForm from "./components/AddRecipeForm";
-import RecipeDetails from "./components/RecipeDetails";
-import FavoritesList from "./components/FavoritesList";
-import RecipeList from "./components/RecipeList";
-
-
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AddRecipeForm />} />
-          <Route path="/recipe" element={<RecipeList />} />
-          <Route path="/favorites" element={<FavoritesList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <h2>Quiz App</h2>
     </>
   )
 }
